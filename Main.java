b99f68e9fe50839b7b2ec07d1ff5a83f73695d26
Monkey@DELL <monkey-hyx@qq.com>
2024-09-09 17:32:45 +0800

    Generally update from dell@tsawke at 2024/09/09 周一 17:32:45.39

---
+++ Main.java
@@ -0,0 +1,6 @@
+package CS;
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Hello World!\n");
+    }
+}
---
+++ update.bat
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Generally update from dell@tsawke at %DATE% %TIME%"
+git push
